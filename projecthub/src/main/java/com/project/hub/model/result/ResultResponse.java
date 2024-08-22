db1898e59ed5d476475ba525e7b4316bc9918368
heesane <tees3359@gmail.com>
2024-08-23 02:02:02 +0900

    feat : Response DTO

---
+++ projecthub/src/main/java/com/project/hub/model/result/ResultResponse.java
@@ -0,0 +1,26 @@
+package com.project.hub.model.result;
+
+import com.project.hub.model.type.ResultCode;
+import lombok.Getter;
+
+@Getter
+public class ResultResponse {
+
+  private final String code;
+  private final String message;
+  private final Object data;
+
+  public static ResultResponse of(ResultCode resultCode, Object data) {
+    return new ResultResponse(resultCode, data);
+  }
+
+  public static ResultResponse of(ResultCode resultCode) {
+    return new ResultResponse(resultCode, null);
+  }
+
+  public ResultResponse(ResultCode resultCode, Object data) {
+    this.code = resultCode.getCode();
+    this.message = resultCode.getMessage();
+    this.data = data;
+  }
+}

2041832c140b570d01c9818352f161032a2e7578
heesane <tees3359@gmail.com>
2024-08-26 00:30:21 +0900

    feat : Response DTO

---
+++ projecthub/src/main/java/com/project/hub/model/mapper/ProjectDetail.java
@@ -0,0 +1,39 @@
+package com.project.hub.model.mapper;
+
+import com.project.hub.entity.Projects;
+import com.project.hub.entity.User;
+import com.project.hub.model.type.Skills;
+import com.project.hub.model.type.Tools;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.Getter;
+
+@Getter
+public class ProjectDetail {
+
+  private final String title;
+  private final String subject;
+  private final String feature;
+  private final String contents;
+  private final List<Skills> skills;
+  private final List<Tools> tools;
+  private final String systemArchitectureUrl;
+  private final String erdUrl;
+  private final String githubUrl;
+  private final User user;
+  private final LocalDateTime registeredAt;
+
+  public ProjectDetail(Projects project) {
+    this.title = project.getTitle();
+    this.subject = project.getSubject();
+    this.feature = project.getFeature();
+    this.contents = project.getContents();
+    this.skills = project.getSkills();
+    this.tools = project.getTools();
+    this.systemArchitectureUrl = project.getSystemArchitectureUrl();
+    this.erdUrl = project.getErdUrl();
+    this.githubUrl = project.getGithubUrl();
+    this.user = project.getUser();
+    this.registeredAt = project.getRegisteredAt();
+  }
+}

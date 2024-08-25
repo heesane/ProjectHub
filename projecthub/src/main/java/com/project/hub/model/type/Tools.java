d954ab5d467d8dff87c4ae34b903ebdc6a050044
heesane <tees3359@gmail.com>
2024-08-26 00:27:19 +0900

    feat : Project Entity Enum Type

---
+++ projecthub/src/main/java/com/project/hub/model/type/Tools.java
@@ -0,0 +1,20 @@
+package com.project.hub.model.type;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum Tools {
+  SLACK("Slack"),
+  NOTION("Notion"),
+  JIRA("Jira"),
+  TRELLO("Trello"),
+  ZOOM("Zoom"),
+  GOOGLE_MEET("Google Meet"),
+  MICROSOFT_TEAMS("Microsoft Teams"),
+  DISCORD("Discord"),
+  ;
+
+  private final String name;
+}

a1995fc262563b954bdfd601fd7d982577a897e8
heesane <tees3359@gmail.com>
2024-08-29 03:01:15 +0900

    feat : Scheduling 활성화

--- projecthub/src/main/java/com/project/hub/ProjecthubApplication.java
+++ projecthub/src/main/java/com/project/hub/ProjecthubApplication.java
@@ -2,8 +2,10 @@ package com.project.hub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ProjecthubApplication {
 
 	public static void main(String[] args) {

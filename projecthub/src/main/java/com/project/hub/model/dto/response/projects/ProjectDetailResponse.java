b72716f70c0759965cb9025892cd2d27e614a667
heesane <tees3359@gmail.com>
2024-08-29 20:57:27 +0900

    refactor : 생성자 어노테이션

--- projecthub/src/main/java/com/project/hub/model/dto/response/projects/ProjectDetailResponse.java
+++ projecthub/src/main/java/com/project/hub/model/dto/response/projects/ProjectDetailResponse.java
@@ -1,15 +1,13 @@
 package com.project.hub.model.dto.response.projects;
 
 import com.project.hub.model.mapper.ProjectDetail;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class ProjectDetailResponse {
 
   private final ProjectDetail projectDetail;
-
-  public ProjectDetailResponse(ProjectDetail projectDetail) {
-    this.projectDetail = projectDetail;
-  }
 
 }

1ec108b3eeb792ea7f05d78b49217e6c157b432c
heesane <tees3359@gmail.com>
2024-08-29 20:57:40 +0900

    feat : ElasticsearchConfig

---
+++ projecthub/src/main/java/com/project/hub/config/ElasticsearchConfig.java
@@ -0,0 +1,22 @@
+package com.project.hub.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Configuration
+@EnableElasticsearchRepositories(basePackages = "com.project.hub.repository.document")
+public class ElasticsearchConfig extends ElasticsearchConfiguration {
+
+  @Value("${spring.elasticsearch.uris[1]}")
+  private String uri;
+
+  @Override
+  public ClientConfiguration clientConfiguration() {
+    return ClientConfiguration.builder()
+        .connectedTo(uri)
+        .build();
+  }
+}

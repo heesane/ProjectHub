acf289d6e7896277758a505ae8053b9f50bcdd2d
heesane <tees3359@gmail.com>
2024-09-01 17:02:36 +0900

    refactor : 단건 검색과 유사 키워드 검색

--- projecthub/src/main/java/com/project/hub/service/SearchService.java
+++ projecthub/src/main/java/com/project/hub/service/SearchService.java
@@ -1,9 +1,18 @@
 package com.project.hub.service;
 
 import com.project.hub.model.documents.ProjectDocuments;
+import com.project.hub.model.mapper.ProjectDetail;
+import java.util.List;
 
 public interface SearchService {
+
+  List<ProjectDocuments> searchProjectByTitleLike(String keyword);
+
   ProjectDocuments searchProjectByTitle(String keyword);
+
+  List<ProjectDetail> searchProjectByTitleInRDBLike(String keyword);
+
+  ProjectDetail searchProjectByTitleInRDB(String keyword);
 
   Object findAllInElasticSearch();
 }

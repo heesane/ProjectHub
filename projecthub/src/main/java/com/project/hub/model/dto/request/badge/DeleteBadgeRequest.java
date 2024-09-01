de46e1449d4c5e63c135103feb731d209ee50255
heesane <tees3359@gmail.com>
2024-09-02 01:51:07 +0900

    feat : Badge Request DTO

---
+++ projecthub/src/main/java/com/project/hub/model/dto/request/badge/DeleteBadgeRequest.java
@@ -0,0 +1,10 @@
+package com.project.hub.model.dto.request.badge;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class DeleteBadgeRequest {
+  private final Long id;
+}

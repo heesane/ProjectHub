c2812b72a384ecbb9bf7d40b00ab57e46af56fdc
heesane <tees3359@gmail.com>
2024-09-01 22:39:19 +0900

    refactor : 예외 처리 적용 및 로직 개선

--- projecthub/src/main/java/com/project/hub/service/impl/UserCommentsService.java
+++ projecthub/src/main/java/com/project/hub/service/impl/UserCommentsService.java
@@ -3,18 +3,20 @@ package com.project.hub.service.impl;
 import com.project.hub.entity.Comments;
 import com.project.hub.entity.Projects;
 import com.project.hub.entity.User;
+import com.project.hub.exceptions.ExceptionCode;
+import com.project.hub.exceptions.exception.NotFoundException;
+import com.project.hub.exceptions.exception.UnmatchedUserException;
+import com.project.hub.model.documents.ProjectDocuments;
 import com.project.hub.model.dto.request.comments.DeleteCommentRequest;
 import com.project.hub.model.dto.request.comments.UpdateCommentRequest;
 import com.project.hub.model.dto.request.comments.WriteCommentRequest;
 import com.project.hub.model.dto.response.ResultResponse;
 import com.project.hub.model.type.ResultCode;
+import com.project.hub.repository.document.ProjectDocumentsRepository;
 import com.project.hub.repository.jpa.CommentsRepository;
-import com.project.hub.repository.jpa.ProjectRepository;
 import com.project.hub.service.CommentsService;
 import com.project.hub.util.UpdateManager;
-import com.project.hub.validator.CommentValidator;
-import com.project.hub.validator.ProjectValidator;
-import com.project.hub.validator.UserValidator;
+import com.project.hub.validator.Validator;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -25,17 +27,20 @@ import org.springframework.stereotype.Service;
 public class UserCommentsService implements CommentsService {
 
   private final CommentsRepository commentsRepository;
-  private final ProjectRepository projectRepository;
-  private final UserValidator userValidator;
-  private final ProjectValidator projectValidator;
-  private final CommentValidator commentValidator;
+  private final ProjectDocumentsRepository projectDocumentsRepository;
+  private final Validator validator;
 
   @Override
   public ResultResponse createComment(WriteCommentRequest request) {
 
-    User commentUser = userValidator.validateAndGetUser(request.getUserId());
+    User commentUser = validator.validateAndGetUser(request.getUserId());
 
-    Projects commentedProject = projectValidator.validateAndGetProject(request.getProjectId());
+    Projects commentedProject = validator.validateAndGetProject(request.getProjectId());
+
+    ProjectDocuments projectDocuments = projectDocumentsRepository.findById(
+        commentedProject.getId()).orElseThrow(
+        () -> new NotFoundException(ExceptionCode.PROJECT_DOCUMENTS_NOT_FOUND)
+    );
 
     // 부모 댓글이 있는 경우 부모 댓글이 존재하는지 확인
     Long parentCommentId = request.getParentCommentId();
@@ -44,9 +49,9 @@ public class UserCommentsService implements CommentsService {
 
     // 대댓글인 경우
     if (parentCommentId != null) {
-      commentValidator.isCommentExist(parentCommentId);
+      validator.isCommentExist(parentCommentId);
 
-      Comments parentComment = commentValidator.validateAndGetComment(parentCommentId);
+      Comments parentComment = validator.validateAndGetComment(parentCommentId);
 
       newComments = Comments.builder()
           .user(commentUser)
@@ -55,12 +60,12 @@ public class UserCommentsService implements CommentsService {
           .parentComment(parentComment)
           .build();
 
-      UpdateManager.incrementProjectCommentCount(commentedProject);
-
-      parentComment.reply(newComments);
+      UpdateManager.incrementProjectCommentCount(projectDocuments, commentedProject);
 
       commentsRepository.save(newComments);
-      projectRepository.save(commentedProject);
+
+      projectDocumentsRepository.save(projectDocuments);
+
       return ResultResponse.of(ResultCode.COMMENT_REPLY_SUCCESS, newComments.getId());
     }
     // 댓글인 경우
@@ -71,10 +76,12 @@ public class UserCommentsService implements CommentsService {
           .contents(request.getContents())
           .build();
 
-      UpdateManager.incrementProjectCommentCount(commentedProject);
+      UpdateManager.incrementProjectCommentCount(projectDocuments, commentedProject);
 
       commentsRepository.save(newComments);
-      projectRepository.save(commentedProject);
+
+      projectDocumentsRepository.save(projectDocuments);
+
       return ResultResponse.of(ResultCode.COMMENT_WRITE_SUCCESS, newComments.getId());
     }
   }
@@ -82,11 +89,11 @@ public class UserCommentsService implements CommentsService {
   @Override
   public ResultResponse updateComment(UpdateCommentRequest request) {
 
-    userValidator.isUserExist(request.getUserId());
+    validator.isUserExist(request.getUserId());
 
-    projectValidator.isProjectExist(request.getProjectId());
+    validator.isProjectExist(request.getProjectId());
 
-    Comments oldComment = commentValidator.validateAndGetComment(request.getCommentId());
+    Comments oldComment = validator.validateAndGetComment(request.getCommentId());
 
     oldComment.update(request);
 
@@ -96,12 +103,12 @@ public class UserCommentsService implements CommentsService {
   @Override
   public ResultResponse deleteComment(DeleteCommentRequest request) {
 
-    userValidator.isUserExist(request.getUserId());
+    validator.isUserExist(request.getUserId());
 
-    Comments oldComments = commentValidator.validateAndGetComment(request.getCommentId());
+    Comments oldComments = validator.validateAndGetComment(request.getCommentId());
 
-    if(!oldComments.getUser().getId().equals(request.getUserId())){
-      throw new IllegalArgumentException("댓글 작성자만 삭제할 수 있습니다.");
+    if (!oldComments.getUser().getId().equals(request.getUserId())) {
+      throw new UnmatchedUserException();
     }
 
     commentsRepository.delete(oldComments);

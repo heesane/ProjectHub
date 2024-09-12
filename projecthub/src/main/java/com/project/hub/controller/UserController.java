b4bdf32c131b7bfe9127bb4b3589fd3a6767df7f
heesane <tees3359@gmail.com>
2024-09-12 22:52:09 +0900

    refactor : Controller Swagger 작성

--- projecthub/src/main/java/com/project/hub/controller/UserController.java
+++ projecthub/src/main/java/com/project/hub/controller/UserController.java
@@ -5,6 +5,8 @@ import com.project.hub.model.dto.request.user.UpdateUserProjectVisibleRequest;
 import com.project.hub.model.dto.response.ResultResponse;
 import com.project.hub.model.type.ResultCode;
 import com.project.hub.service.impl.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "UserController", description = "유저 Profile API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/user")
@@ -22,16 +25,28 @@ public class UserController {
 
   private final UserService userService;
 
+  @Operation(
+      summary = "Profile 조회",
+      description = "유저 프로필 조회"
+  )
   @GetMapping("/profile/{userId}")
   public ResponseEntity<ResultResponse> getProfile(@PathVariable Long userId) {
     return ResponseEntity.ok(ResultResponse.of(ResultCode.USER_PROFILE_SUCCESS,userService.myProfile(userId)));
   }
 
+  @Operation(
+      summary = "Profile 수정",
+      description = "유저 프로필 수정"
+  )
   @PatchMapping("/update")
   public ResponseEntity<ResultResponse> updateProfile(@RequestBody UpdateUserProfileRequest request) {
     return ResponseEntity.ok(ResultResponse.of(ResultCode.USER_UPDATE_SUCCESS,userService.changeNickname(request)));
   }
 
+  @Operation(
+      summary = "프로젝트 공개 여부 수정",
+      description = "프로젝트 공개 여부 수정"
+  )
   @PostMapping("/visible")
   public ResponseEntity<ResultResponse> changeVisible(@RequestBody UpdateUserProjectVisibleRequest request) {
     return ResponseEntity.ok(ResultResponse.of(ResultCode.USER_VISIBLE_SUCCESS,userService.changeVisible(request)));
--- projecthub/src/main/java/com/project/hub/controller/UserLikeController.java
+++ projecthub/src/main/java/com/project/hub/controller/UserLikeController.java
@@ -6,6 +6,8 @@ import com.project.hub.model.dto.request.likes.ProjectLikeRequest;
 import com.project.hub.model.dto.response.ResultResponse;
 import com.project.hub.model.dto.response.comments.Comment;
 import com.project.hub.service.impl.UserLikeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "UserLikeController", description = "좋아요 API")
 @RestController
 @RequestMapping("/api/v1/like")
 @RequiredArgsConstructor
@@ -21,11 +24,19 @@ public class UserLikeController {
 
   private final UserLikeService userLikeService;
 
+  @Operation(
+      summary = "좋아요",
+      description = "프로젝트, 댓글 좋아요"
+  )
   @PostMapping("/project")
   public ResponseEntity<ResultResponse> projectLike(@RequestBody @Valid ProjectLikeRequest request){
     return ResponseEntity.ok(userLikeService.like(request));
   }
 
+  @Operation(
+      summary = "댓글 좋아요",
+      description = "댓글 좋아요"
+  )
   @PostMapping("/comment")
   public ResponseEntity<ResultResponse> commentLike(@RequestBody @Valid CommentLikeRequest request){
     return ResponseEntity.ok(userLikeService.like(request));

fdacb8e316fd2ab920401982e728988e9308fb8f
heesane <tees3359@gmail.com>
2024-09-18 23:45:05 +0900

    refactor : Spring Security 수정

--- projecthub/src/main/java/com/project/hub/config/SecurityConfig.java
+++ projecthub/src/main/java/com/project/hub/config/SecurityConfig.java
@@ -1,22 +1,30 @@
 package com.project.hub.config;
 
-import com.project.hub.auth.jwt.filter.JwtTokenFilter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.hub.auth.jwt.LoginService;
+import com.project.hub.auth.jwt.filter.CustomJsonUsernamePasswordAuthenticationFilter;
+import com.project.hub.auth.jwt.filter.JwtAuthenticationProcessingFilter;
+import com.project.hub.auth.jwt.handler.LoginFailureHandler;
+import com.project.hub.auth.jwt.handler.LoginSuccessHandler;
 import com.project.hub.auth.oauth.CustomOAuth2UserService;
 import com.project.hub.auth.oauth.handler.OAuth2FailureHandler;
 import com.project.hub.auth.oauth.handler.OAuth2SuccessHandler;
-import com.project.hub.auth.service.TokenComponent;
-import com.project.hub.service.AuthService;
+import com.project.hub.auth.service.TokenService;
+import com.project.hub.repository.jpa.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 
 @Configuration
 @RequiredArgsConstructor
@@ -26,25 +34,31 @@ public class SecurityConfig {
   private final CustomOAuth2UserService customOAuth2UserService;
   private final OAuth2SuccessHandler oAuth2SuccessHandler;
   private final OAuth2FailureHandler oAuth2FailureHandler;
-  private final TokenComponent tokenComponent;
-  private final AuthService authService;
+  private final BCryptPasswordEncoder bCryptPasswordEncoder;
+  private final TokenService tokenService;
+  private final LoginService loginService;
+  private final UserRepository userRepository;
+  private final ObjectMapper objectMapper;
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
+        .formLogin(AbstractHttpConfigurer::disable)
         .httpBasic(AbstractHttpConfigurer::disable)
         .csrf(AbstractHttpConfigurer::disable)
-        .formLogin(AbstractHttpConfigurer::disable)
         .headers(headers -> headers.frameOptions(
             HeadersConfigurer.FrameOptionsConfig::sameOrigin))
         .authorizeHttpRequests(requests ->
-            requests
-                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                .requestMatchers("/**").permitAll()
+                requests.requestMatchers("/**").permitAll()
+//            requests
+//                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+//                .requestMatchers("/v3/**","swagger-ui/**").permitAll()
+//                .requestMatchers("/api/v1/auth/login","/api/v1/auth/register").permitAll()
+//                .requestMatchers("/api/v1/user/profile/**").authenticated()
         )
         .sessionManagement(sessionManagement ->
             sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        )  // 세션을 사용하지 않으므로 STATELESS 설정
+        )
         .oauth2Login(oauth2Login -> oauth2Login
             .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint
                 .userService(customOAuth2UserService)
@@ -52,12 +66,45 @@ public class SecurityConfig {
             .successHandler(oAuth2SuccessHandler)
             .failureHandler(oAuth2FailureHandler)
         )
-        .addFilterAfter(this.jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        .addFilterAfter(customJsonUsernamePasswordAuthenticationFilter(), LogoutFilter.class)
+        .addFilterBefore(jwtAuthenticationProcessingFilter(),
+            CustomJsonUsernamePasswordAuthenticationFilter.class);
 
     return http.build();
   }
 
-  private JwtTokenFilter jwtTokenFilter() {
-    return new JwtTokenFilter(tokenComponent, authService);
+  @Bean
+  public AuthenticationManager authenticationManager() {
+    DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+    provider.setPasswordEncoder(bCryptPasswordEncoder);
+    provider.setUserDetailsService(loginService);
+    return new ProviderManager(provider);
+  }
+
+  @Bean
+  public LoginSuccessHandler loginSuccessHandler() {
+    return new LoginSuccessHandler(tokenService, userRepository);
+  }
+
+  @Bean
+  public LoginFailureHandler loginFailureHandler() {
+    return new LoginFailureHandler();
+  }
+
+  @Bean
+  public CustomJsonUsernamePasswordAuthenticationFilter customJsonUsernamePasswordAuthenticationFilter() {
+    CustomJsonUsernamePasswordAuthenticationFilter filter = new CustomJsonUsernamePasswordAuthenticationFilter(
+        objectMapper);
+    filter.setAuthenticationManager(authenticationManager());
+    filter.setFilterProcessesUrl("/api/v1/auth/login");
+    filter.setAuthenticationSuccessHandler(loginSuccessHandler());
+    filter.setAuthenticationFailureHandler(loginFailureHandler());
+    return filter;
+  }
+
+  @Bean
+  public JwtAuthenticationProcessingFilter jwtAuthenticationProcessingFilter() {
+    return new JwtAuthenticationProcessingFilter(
+        tokenService, userRepository);
   }
 }

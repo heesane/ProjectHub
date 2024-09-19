686d29193ed9f4cc62c1c6e27a42c15be414a920
heesane <tees3359@gmail.com>
2024-09-19 23:23:28 +0900

    refactor : 특정 조건에 따라 조회

--- projecthub/src/main/java/com/project/hub/controller/UserSearchController.java
+++ projecthub/src/main/java/com/project/hub/controller/UserSearchController.java
@@ -4,6 +4,8 @@ import com.project.hub.model.documents.ProjectDocuments;
 import com.project.hub.model.dto.response.ResultResponse;
 import com.project.hub.model.mapper.ProjectDetail;
 import com.project.hub.model.type.ResultCode;
+import com.project.hub.model.type.SearchType;
+import com.project.hub.service.SearchService;
 import com.project.hub.service.impl.UserSearchService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -13,6 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -27,70 +30,64 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserSearchController {
 
-  private final UserSearchService userSearchService;
+  private final SearchService userSearchService;
+
+//  @Operation(
+//      summary = "프로젝트 제목으로 검색",
+//      description = "프로젝트 제목으로 검색합니다."
+//  )
+//  @GetMapping("/es")
+//  public ResponseEntity<ResultResponse> searchProjectByTitle(String keyword) {
+//    ProjectDocuments projectDocuments = userSearchService.searchProjectByTitle(keyword);
+//    return ResponseEntity.ok(ResultResponse.of(ResultCode.SEARCH_SUCCESS,projectDocuments));
+//  }
 
   @Operation(
-      summary = "프로젝트 제목으로 검색",
+      summary = "프로젝트 제목으로 검색 (created, like, commentCount)",
       description = "프로젝트 제목으로 검색합니다."
   )
-  @GetMapping("/es")
-  public ResponseEntity<ResultResponse> searchProjectByTitle(String keyword) {
-    long startTime = System.currentTimeMillis();
-    ProjectDocuments projectDocuments = userSearchService.searchProjectByTitle(keyword);
-    long endTime = System.currentTimeMillis();
-    log.info("searchProjectByTitle elapsed time: {}", endTime - startTime);
-    return ResponseEntity.ok(ResultResponse.of(ResultCode.SEARCH_SUCCESS,projectDocuments));
+  @GetMapping("")
+  public ResponseEntity<ResultResponse> searchProjectByTitleLike(
+      @RequestParam(value="keyword") String keyword,
+      @RequestParam(value="page", defaultValue = "0") int page,
+      @RequestParam(value="size", defaultValue = "10") int size,
+      @RequestParam(value="sort", defaultValue = "id") SearchType sort
+  ) {
+    return ResponseEntity.ok(ResultResponse.of(ResultCode.SEARCH_SUCCESS,userSearchService.searchProjectByTitleLike(keyword, page, size, sort)));
   }
 
   @Operation(
-      summary = "프로젝트 제목으로 검색 (Like)",
-      description = "프로젝트 제목으로 검색합니다."
-  )
-  @GetMapping("/es/like")
-  public ResponseEntity<ResultResponse> searchProjectByTitleLike(String keyword) {
-    long startTime = System.currentTimeMillis();
-    List<ProjectDocuments> projectDocuments = userSearchService.searchProjectByTitleLike(keyword);
-    long endTime = System.currentTimeMillis();
-    log.info("searchProjectByTitleLike elapsed time: {}", endTime - startTime);
-    return ResponseEntity.ok(ResultResponse.of(ResultCode.SEARCH_SUCCESS,projectDocuments));
-  }
-
-  @Operation(
-      summary = "프로젝트 제목으로 검색 (RDB)",
-      description = "프로젝트 제목으로 검색합니다."
-  )
-  @GetMapping("/rdb")
-  public ResponseEntity<ResultResponse> searchProjectByTitleInRDB(String keyword) {
-    long startTime = System.currentTimeMillis();
-    ProjectDetail projectDetail = userSearchService.searchProjectByTitleInRDB(keyword);
-    long endTime = System.currentTimeMillis();
-    log.info("searchProjectByTitleInRDB elapsed time: {}", endTime - startTime);
-    return ResponseEntity.ok(ResultResponse.of(ResultCode.SEARCH_SUCCESS,projectDetail));
-  }
-
-  @Operation(
-      summary = "프로젝트 제목으로 검색 (RDB Like)",
-      description = "프로젝트 제목으로 검색합니다."
-  )
-  @GetMapping("/rdb/like")
-  public ResponseEntity<ResultResponse> searchProjectByTitleInRDBLike(String keyword) {
-    long startTime = System.currentTimeMillis();
-    List<ProjectDetail> projectDetail = userSearchService.searchProjectByTitleInRDBLike(keyword);
-    long endTime = System.currentTimeMillis();
-    log.info("searchProjectByTitleInRDBLike elapsed time: {}", endTime - startTime);
-    return ResponseEntity.ok(ResultResponse.of(ResultCode.SEARCH_SUCCESS,projectDetail));
-  }
-
-  @Operation(
-      summary = "모든 데이터 조회 (Elasticsearch)",
+      summary = "모든 데이터 조회 (created, like, commentCount)",
       description = "모든 데이터를 조회합니다."
   )
-  @GetMapping("/es/all")
-  public ResponseEntity<ResultResponse> findAllInElasticSearch() {
-    long startTime = System.currentTimeMillis();
-    Iterable<ProjectDocuments> projectDocuments = userSearchService.findAllInElasticSearch();
-    long endTime = System.currentTimeMillis();
-    log.info("findAllInElasticSearch elapsed time: {}", endTime - startTime);
-    return ResponseEntity.ok(ResultResponse.of(ResultCode.SEARCH_SUCCESS,projectDocuments));
+  @GetMapping("/all")
+  public ResponseEntity<ResultResponse> findAllInElasticSearch(
+      @RequestParam(value="page", defaultValue = "0") int page,
+      @RequestParam(value="size", defaultValue = "10") int size,
+      @RequestParam(value="sort", defaultValue = "id") SearchType sort
+  ) {
+    return ResponseEntity.ok(ResultResponse.of(ResultCode.SEARCH_SUCCESS,userSearchService.findAllInElasticSearch(page, size, sort)));
   }
+
+//  @Operation(
+//      summary = "프로젝트 제목으로 검색 (RDB)",
+//      description = "프로젝트 제목으로 검색합니다."
+//  )
+//  @GetMapping("/rdb")
+//  public ResponseEntity<ResultResponse> searchProjectByTitleInRDB(String keyword) {
+//    ProjectDetail projectDetail = userSearchService.searchProjectByTitleInRDB(keyword);
+//    return ResponseEntity.ok(ResultResponse.of(ResultCode.SEARCH_SUCCESS,projectDetail));
+//  }
+//
+//  @Operation(
+//      summary = "프로젝트 제목으로 검색 (RDB Like)",
+//      description = "프로젝트 제목으로 검색합니다."
+//  )
+//  @GetMapping("/rdb/like")
+//  public ResponseEntity<ResultResponse> searchProjectByTitleInRDBLike(String keyword) {
+//    List<ProjectDetail> projectDetail = userSearchService.searchProjectByTitleInRDBLike(keyword);
+//    return ResponseEntity.ok(ResultResponse.of(ResultCode.SEARCH_SUCCESS,projectDetail));
+//  }
+
+
 }
--- projecthub/src/main/java/com/project/hub/service/impl/UserSearchService.java
+++ projecthub/src/main/java/com/project/hub/service/impl/UserSearchService.java
@@ -5,13 +5,21 @@ import com.project.hub.exceptions.ExceptionCode;
 import com.project.hub.exceptions.exception.NotFoundException;
 import com.project.hub.model.documents.ProjectDocuments;
 import com.project.hub.model.mapper.ProjectDetail;
+import com.project.hub.model.type.SearchType;
 import com.project.hub.repository.document.ProjectDocumentsRepository;
 import com.project.hub.repository.jpa.ProjectRepository;
 import com.project.hub.service.SearchService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserSearchService implements SearchService {
@@ -27,11 +35,6 @@ public class UserSearchService implements SearchService {
   }
 
   @Override
-  public List<ProjectDocuments> searchProjectByTitleLike(String keyword) {
-    return projectDocumentsRepository.findByTitleLike(keyword);
-  }
-
-  @Override
   public ProjectDetail searchProjectByTitleInRDB(String keyword) {
     return projectRepository.findByTitle(keyword).map(ProjectDetail::new).orElseThrow(
         () -> new NotFoundException(ExceptionCode.PROJECT_NOT_FOUND)
@@ -40,12 +43,9 @@ public class UserSearchService implements SearchService {
 
   @Override
   public List<ProjectDetail> searchProjectByTitleInRDBLike(String keyword) {
-    List<Projects> projects = projectRepository.findAllByTitleLike("%"+keyword+"%");
+    List<Projects> projects = projectRepository.findAllByTitleLike("%" + keyword + "%");
     return projects.stream().map(ProjectDetail::new).toList();
   }
 
-  @Override
-  public Iterable<ProjectDocuments> findAllInElasticSearch() {
-    return projectDocumentsRepository.findAll();
-  }
+
 }
